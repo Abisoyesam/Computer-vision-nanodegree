44af8227d60c860dd35eabccfbe64cbd74e41dc7
Abisoye Akinloye <abisoyesam@gmail.com>
2022-07-27 06:48:43 +0100

    modify utils

--- Lessons/utils.py
+++ Lessons/utils.py
@@ -1,30 +1,34 @@
-def rgb2hsv(R,G,B,scaleFactor=100):
-	'''	
-	To convert rgb color to hsv color space
-	:params: r -> It is the red color code
-	:params: g -> It is the green color code
-	:params: b -> It is the blue color code
-	'''
-	r, g, b = R/255, G/255, B/255
+def rgb2hsv(Red, Green, Blue):
+	"""
+	:param Red: the value of red color ranges from 0 - 255
+	:param Green: the value of green color ranges from 0 - 255
+	:param Blue: the value of blue color ranges from 0 - 255
+	"""
+	r,g,b = Red/255, Green/255, Blue/255
 	vmax = max(r,g,b)
 	vmin = min(r,g,b)
 	diff = vmax - vmin
 
-	if vmax == vmin:
-		h = 0
-	elif vmax == r:
-		h = (60 * ((g-b)/diff)+0)%360
-	elif vmax == g:
-		h = (60 * ((b-r)/diff)+120)%360
-	elif vmax == b:
-		h = (60 * ((r-g)/diff)+240)%360
+	v = vmax
 
-	if h < 0:
-		h = h+360
 	if vmax == 0:
 		s = 0
 	else:
-		s = (diff/vmax)* scaleFactor
+		s = diff/vmax
 
-	v = scaleFactor * vmax
+	if vmax == r:
+		h = (60 * (g-b)/diff) % 360
+		# modulus of 360 b'cos h ranges from 0 - 360
+	elif vmax == g:
+		h =  (120 + (60 * (b-r)/(diff)) % 360)
+	elif vmax == b:
+		h = (240 + (60 * (r-g)/(diff)) % 360)
+
 	return h,s,v
+
+
+
+def load_dataset(dir):
+	pass
+
+print(rgb2hsv(100,200,50))
